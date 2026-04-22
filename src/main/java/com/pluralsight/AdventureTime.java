3bf6f3cffa58c83242a32aec1b48be7fbe37ed89
Sean Zamora <zamoraseans@gmail.com>
2026-04-22 11:14:39 -0700

    added loadAdventure()

--- src/main/java/com/pluralsight/AdventureTime.java
+++ src/main/java/com/pluralsight/AdventureTime.java
@@ -1,9 +1,34 @@
 package com.pluralsight;
+import java.io.*;
+import java.util.*;
 
 public class AdventureTime
 {
     public static void main(String[] args)
     {
+        loadAdventure();
+    }
 
+    public static void loadAdventure()
+    {
+        try
+        {
+            FileReader fileReader = new FileReader("adventure1.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line = bufferedReader.readLine();
+
+            while (line != null)
+            {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+            fileReader.close();
+        }
+        catch (Exception e)
+        {
+
+        }
     }
 }
